05ca08c3f06eefdef7af5043db151aeeebf2d3fb
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-02-27 14:34:13 -0300

    up

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4b7lhdd4kog"
+    "revision": "0.22vd08gs7oo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
